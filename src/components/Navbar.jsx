b80b8478a9a27444a3a44dbedb6e5583c7b710a9
Camilecacao <cami.nyu@gmail.com>
2024-05-19 00:17:52 -0400

    se agrega a nav cantidad y precio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+'use client'
 import Link from 'next/link';
 import BotonPanel from './BotonPanel';
+import React, {useContext} from 'react';
+import {CartContext} from "@/contexts/CartContext";
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const { cartItems } = useContext(CartContext);
+    const cantidadTotal = cartItems.reduce((total, item) => total + item.quantity, 0);
+
+    // Calcular la suma total del precio de todos los ítems en el carrito
+    const precioTotal = cartItems.reduce((total, item) => total + (item.precio * item.quantity), 0);
+
     return (
         <div className="navbar bg-white">
+
             <div className="navbar-start mr-10">
             <div className='pl-20'>
                 <div className="hidden sm:block"> {/* Oculto en pantallas pequeñas */}
@@ -31,13 +40,13 @@ const Navbar = () => {
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
                         <div className="indicator">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-                            <span className="badge badge-sm indicator-item">8</span>
+                            <span className="badge badge-sm indicator-item">{cantidadTotal}</span>
                         </div>
                     </div>
                     <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
                         <div className="card-body">
-                            <span className="font-bold text-lg">8 Items</span>
-                            <span className="text-info">Subtotal: $999</span>
+                            <span className="font-bold text-lg">{cantidadTotal} Items</span>
+                            <span className="text-info">Subtotal: ${precioTotal}</span>
                             <Link href='/viewCart'>
                             <div className="card-actions">
                                 <button className="btn btn-primary btn-block">View cart</button>
