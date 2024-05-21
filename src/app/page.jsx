ce85d219dfd70fa1572f0b3bf995260cb8654ffb
unknown <nicolasgonzalezmoller@gmail.com>
2024-05-20 23:13:26 -0400

    lo vendido

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import Categorias from "@/components/Categorias";
+import LoMasVendido from "@/components/LoMasVendido";
 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
       <Categorias />
+      <LoMasVendido />
       
 
 
