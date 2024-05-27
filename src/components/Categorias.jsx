b3c2d8ee0bfb4699375d642160185cb56f6f1c08
unknown <nicolasgonzalezmoller@gmail.com>
2024-05-26 21:31:07 -0400

    cards de categorias

--- src/components/Categorias.jsx
+++ src/components/Categorias.jsx
@@ -1,68 +1,74 @@
 import Link from 'next/link'
 import React from 'react'
+import LoMasVendido from './LoMasVendido'
 
 const Categorias = () => {
     return (
-        <div className="flex flex-wrap justify-around mt-24">
-            {/* Tarjeta 1: Niños ELA */}
-            <Link href='ninosEla'>
-            <div className="w-64 mx-2 mb-4 bg-white shadow-lg rounded-lg overflow-hidden">
-                <img className="w-full h-40 object-cover" src="https://res.cloudinary.com/dpbpyzl96/image/upload/v1714528155/arthat/e1p6oyfnxzwnvohkfvkc.jpg" alt="Imagen Niños ELA" />
-                <div className="px-4 py-2">
-                    <h3 className="text-gray-800 font-semibold text-lg">Niños ELA</h3>
-                </div>
-            </div>
-            </Link>
+        <div>
+            <div className="flex flex-wrap justify-around mt-24 mb-40 gap-2 ">
+                {/* Tarjeta 1: Niños ELA */}
+                <Link href='ninosEla'>
+                    <div className="w-64 mx-2 mb-4 bg-white shadow-lg rounded-lg overflow-hidden">
+                        <img className="w-full h-40 object-contain" src="https://res.cloudinary.com/dpbpyzl96/image/upload/v1714528155/arthat/e1p6oyfnxzwnvohkfvkc.jpg" alt="Imagen Niños ELA" />
+                        <div className="px-4 py-2">
+                            <h3 className="text-gray-800 font-semibold text-lg">Niños ELA</h3>
+                        </div>
+                    </div>
+                </Link>
 
-            {/* Tarjeta 2: Anillos de Hombre */}
-            <Link href='anillosHombre'>
-            <div className="w-64 mx-2 mb-4 bg-white shadow-lg rounded-lg overflow-hidden">
-                <img className="w-full h-40 object-cover" src="https://media.istockphoto.com/id/1314987155/es/foto/pulseras-doradas-modernas-y-anillo-sobre-fondo-blanco-y-azul-con-espacio-de-copia.jpg?s=612x612&w=0&k=20&c=bSbzUGZROloXDbrHmMPL7tHJG8FBrit75DLCzYNTNAI=" alt="Imagen Anillos de Hombre" />
-                <div className="px-4 py-2">
-                    <h3 className="text-gray-800 font-semibold text-lg">Anillos de Hombre</h3>
-                </div>
-            </div>
-            </Link>
 
-            {/* Tarjeta 3: Madres y Niños */}
-            <Link href='madresYninos'>
-            <div className="w-64 mx-2 mb-4 bg-white shadow-lg rounded-lg overflow-hidden">
-                <img className="w-full h-40 object-cover" src="https://res.cloudinary.com/dpbpyzl96/image/upload/v1714528155/arthat/jynwtqjsqsnoqv8bi1ms.jpg" alt="Imagen Madres y Niños" />
-                <div className="px-4 py-2">
-                    <h3 className="text-gray-800 font-semibold text-lg">Madres y Niños</h3>
-                </div>
-            </div>
-            </Link>
+                {/* Tarjeta 2: Anillos de Hombre */}
+                <Link href='anillosHombre'>
+                    <div className="w-64 mx-2 mb-4 bg-white shadow-lg rounded-lg overflow-hidden">
+                        <img className="w-full h-40 object-contain" src="https://media.istockphoto.com/id/1314987155/es/foto/pulseras-doradas-modernas-y-anillo-sobre-fondo-blanco-y-azul-con-espacio-de-copia.jpg?s=612x612&w=0&k=20&c=bSbzUGZROloXDbrHmMPL7tHJG8FBrit75DLCzYNTNAI=" alt="Imagen Anillos de Hombre" />
+                        <div className="px-4 py-2">
+                            <h3 className="text-gray-800 font-semibold text-lg">Anillos de Hombre</h3>
+                        </div>
+                    </div>
+                </Link>
 
-            {/* Tarjeta 4: Amuletos */}
-            <Link href='/amuletos'>
-            <div className="w-64 mx-2 mb-4 bg-white shadow-lg rounded-lg overflow-hidden">
-                <img className="w-full h-40 object-cover" src="https://res.cloudinary.com/dpbpyzl96/image/upload/v1714528154/arthat/hux0ubv47am4kdd0hqrs.jpg" alt="Imagen Amuletos" />
-                <div className="px-4 py-2">
-                    <h3 className="text-gray-800 font-semibold text-lg">Amuletos</h3>
-                </div>
-            </div>
-            </Link>
+                {/* Tarjeta 3: Madres y Niños */}
+                <Link href='madresYninos'>
+                    <div className="w-64 mx-2 mb-4 bg-white shadow-lg rounded-lg overflow-hidden">
+                        <img className="w-full h-40 object-contain" src="https://res.cloudinary.com/dpbpyzl96/image/upload/v1714528155/arthat/jynwtqjsqsnoqv8bi1ms.jpg" alt="Imagen Madres y Niños" />
+                        <div className="px-4 py-2">
+                            <h3 className="text-gray-800 font-semibold text-lg">Madres y Niños</h3>
+                        </div>
+                    </div>
+                </Link>
 
-            {/* Tarjeta 5: Runas */}
-            <Link href='/runas'>
-            <div className="w-64 mx-2 mb-4 bg-white shadow-lg rounded-lg overflow-hidden">
-                <img className="w-full h-40 object-cover" src="https://res.cloudinary.com/dpbpyzl96/image/upload/v1714528155/arthat/ajubizwvacj9elahpohq.jpg" alt="Imagen Runas" />
-                <div className="px-4 py-2">
-                    <h3 className="text-gray-800 font-semibold text-lg">Runas</h3>
-                </div>
-            </div>
-            </Link>
+                {/* Tarjeta 4: Amuletos */}
+                <Link href='/amuletos'>
+                    <div className="w-64 mx-2 mb-4 bg-white shadow-lg rounded-lg overflow-hidden">
+                        <img className="w-full h-40 object-contain" src="https://res.cloudinary.com/dpbpyzl96/image/upload/v1714528154/arthat/hux0ubv47am4kdd0hqrs.jpg" alt="Imagen Amuletos" />
+                        <div className="px-4 py-2">
+                            <h3 className="text-gray-800 font-semibold text-lg">Amuletos</h3>
+                        </div>
+                    </div>
+                </Link>
 
-            {/* Tarjeta 6: Profesiones */}
-            <Link href='profesiones'>
-            <div className="w-64 mx-2 mb-4 bg-white shadow-lg rounded-lg overflow-hidden">
-                <img className="w-full h-40 object-cover" src="https://res.cloudinary.com/dpbpyzl96/image/upload/v1714528155/arthat/vaztbzxbsrjm5vbilr72.jpg" alt="Imagen Profesiones" />
-                <div className="px-4 py-2">
-                    <h3 className="text-gray-800 font-semibold text-lg">Profesiones</h3>
-                </div>
+                {/* Tarjeta 5: Runas */}
+                <Link href='/runas'>
+                    <div className="w-64 mx-2 mb-4 bg-white shadow-lg rounded-lg overflow-hidden">
+                        <img className="w-full h-40 object-contain" src="https://res.cloudinary.com/dpbpyzl96/image/upload/v1714528155/arthat/ajubizwvacj9elahpohq.jpg" alt="Imagen Runas" />
+                        <div className="px-4 py-2">
+                            <h3 className="text-gray-800 font-semibold text-lg">Runas</h3>
+                        </div>
+                    </div>
+                </Link>
+
+                {/* Tarjeta 6: Profesiones */}
+                <Link href='profesiones'>
+                    <div className="w-64 mx-2 mb-4 bg-white shadow-lg rounded-lg overflow-hidden">
+                        <img className="w-full h-40 object-contain" src="https://res.cloudinary.com/dpbpyzl96/image/upload/v1714528155/arthat/vaztbzxbsrjm5vbilr72.jpg" alt="Imagen Profesiones" />
+                        <div className="px-4 py-2">
+                            <h3 className="text-gray-800 font-semibold text-lg">Profesiones</h3>
+                        </div>
+                    </div>
+                </Link>
+
             </div>
-            </Link>
+            <LoMasVendido />
         </div>
 
 
--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,5 +1,5 @@
 'use client'
-import {createContext, useReducer} from "react";
+import { createContext, useReducer } from "react";
 
 export const CartContext = createContext();
 
@@ -19,49 +19,49 @@ function reducer(state, action) {
                     ...updatedCartItems[existingItemIndex],
                     quantity: updatedCartItems[existingItemIndex].quantity + 1
                 };
-                return {...state, cartItems: updatedCartItems};
+                return { ...state, cartItems: updatedCartItems };
             } else {
                 // Si es un nuevo producto, agrégalo al carrito
-                return {...state, cartItems: [...state.cartItems, {...action.item, quantity: 1}]};
+                return { ...state, cartItems: [...state.cartItems, { ...action.item, quantity: 1 }] };
             }
 
-     
+
 
 
 
         case 'REMOVE_ITEM':
             const existingItem = state.cartItems.find(item => item._id === action.id);
-                if (existingItem.quantity > 1) {
-                    // Si la cantidad del producto es mayor que 1, reducimos la cantidad en 1
-                    const updatedCartItems = state.cartItems.map(item =>
-                        item._id === action.id ? {...item, quantity: item.quantity - 1} : item
-                    );
-                    return {...state, cartItems: updatedCartItems};
-                } else {
-                    // Si la cantidad del producto es 1, lo eliminamos del carrito
-                    const newCartItems = state.cartItems.filter(item => item._id !== action.id);
-                    return {...state, cartItems: newCartItems};
-                }
+            if (existingItem.quantity > 1) {
+                // Si la cantidad del producto es mayor que 1, reducimos la cantidad en 1
+                const updatedCartItems = state.cartItems.map(item =>
+                    item._id === action.id ? { ...item, quantity: item.quantity - 1 } : item
+                );
+                return { ...state, cartItems: updatedCartItems };
+            } else {
+                // Si la cantidad del producto es 1, lo eliminamos del carrito
+                const newCartItems = state.cartItems.filter(item => item._id !== action.id);
+                return { ...state, cartItems: newCartItems };
+            }
         default:
 
             return state;
     }
 }
 
-export function CartContextProvider({children}) {
+export function CartContextProvider({ children }) {
     const [state, dispatch] = useReducer(reducer, initialState);
 
     const addItem = (item) => {
-        dispatch({type: 'ADD_ITEM', item});
+        dispatch({ type: 'ADD_ITEM', item });
     };
 
-   
+
     const removeItem = (id) => {
-        dispatch({type: 'REMOVE_ITEM', id});
+        dispatch({ type: 'REMOVE_ITEM', id });
     };
 
     return (
-        <CartContext.Provider value={{cartItems: state.cartItems, addItem, removeItem}}>
+        <CartContext.Provider value={{ cartItems: state.cartItems, addItem, removeItem }}>
             {children}
         </CartContext.Provider>
     );
