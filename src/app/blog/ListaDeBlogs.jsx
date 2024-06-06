7a142aa7e04a8e8b0165246d48dd083c7348afd0
unknown <nicolasgonzalezmoller@gmail.com>
2024-06-05 22:08:21 -0400

    blog post

--- src/app/blog/ListaDeBlogs.jsx
+++ src/app/blog/ListaDeBlogs.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+'use client'
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
 
 const ListaDeBlogs = () => {
+    const [blogList, setBlogList] = useState([]);
+
+    useEffect(() => {
+        fetch('https://backendjwl.onrender.com/blogs')
+            .then(response => response.json())
+            .then(data => setBlogList(data.info))
+            .catch(error => console.error('Error al hacer la solicitud', error));
+    }, []);
+
     return (
         <div className='container'>
             <div className='text-start text-black text-2xl font-bold'>BLOG</div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                {blogList.map(blog => (
+                    <Link href={`/blogs/${blog._id}`} key={blog._id}>
+                        <div className='my-4 p-4 border border-gray-300 rounded-lg'>
+                            <h1 className='text-xl font-bold'>{blog.titulo}</h1>
+                            <div className='my-2'>
+                                <img src={blog.img} alt={blog.titulo} className='w-full h-auto' />
+                            </div>
+                            <div className='my-2'>
+                                <p>{blog.contenido}</p>
+                            </div>
+                        </div>
+                    </Link>
+                ))}
+            </div>
         </div>
+
     );
 }
 
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
+
 const page = () => {
     return (
         <div>
 
             <div className='text-lg text-center'> Articulos</div>
+            
 
         </div>
     )
